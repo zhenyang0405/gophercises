ec22d803c893a9223fb4e912b047f696ebe7e33f
Zhen Yang <41467740+zhenyang0405@users.noreply.github.com>
2021-10-04 16:06:36 +0800

    Create bolt database at homedir

--- cli-task-manager/go.mod
+++ cli-task-manager/go.mod
@@ -2,4 +2,8 @@ module github.com/zhenyang0405/gophercises/cli-task-manager
 
 go 1.16
 
-require github.com/spf13/cobra v1.2.1 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+)
--- cli-task-manager/main.go
+++ cli-task-manager/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/zhenyang0405/gophercises/cli-task-manager/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/zhenyang0405/gophercises/cli-task-manager/cmd"
+	db "github.com/zhenyang0405/gophercises/cli-task-manager/database"
+)
 
 func main() {
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "cli-task-manager.db")
+	err := db.Init(dbPath)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 	cmd.RootCmd.Execute()
 }
